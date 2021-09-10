0f2bc6956f50e904c15397d94d1745baeffd09ff
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-09-10 11:30:53 +0200

    new scss

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,21 +1,2 @@
 import './src/global.js';
 import './src/smoothscroll.js';
-import Icon from '../icons/icon.png';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello webpack';
-  element.classList.add('hello');
-
- // Add the image to our existing div.
- const myIcon = new Image();
- myIcon.src = Icon;
-
- element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
