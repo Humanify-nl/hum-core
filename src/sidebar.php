3e2e6cef2fc858fac21eccc03ac9ac24dfcaa682
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-09-12 12:16:25 +0200

    new sidebar

---
+++ src/sidebar.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Sidebar
+ *
+ * @package      EAStarter
+ * @author       Bill Erickson
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+if ( ! function_exists( 'hum_page_layout' ) )
+	return;
+
+$layout = hum_page_layout();
+if ( ! in_array( $layout, array( 'content-sidebar', 'sidebar-content' ) ) )
+	return;
+
+$sidebar = apply_filters( 'hum_sidebar', 'primary-sidebar' );
+$display = is_active_sidebar( $sidebar );
+if ( ! apply_filters( 'hum_display_sidebar', $display ) )
+	return;
+
+tha_sidebars_before();
+
+echo '<aside class="sidebar-primary" role="complementary">';
+
+	tha_sidebar_top();
+	if ( is_active_sidebar( $sidebar ) ) {
+		dynamic_sidebar( $sidebar );
+	}
+	tha_sidebar_bottom();
+
+echo '</aside>';
+
+tha_sidebars_after();
