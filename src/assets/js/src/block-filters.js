12d63b199b6e3d34fc0bf4a4e85936d6c374ccb4
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-11-03 15:26:43 +0100

    filter settings

--- src/assets/js/src/block-filters.js
+++ src/assets/js/src/block-filters.js
@@ -23,3 +23,71 @@ wp.hooks.addFilter(
       return settings;
   }
 );
+
+
+// remove text color for some blocks
+
+wp.hooks.addFilter(
+  'blocks.registerBlockType',
+  'hum/colors',
+  function( settings, name ) {
+
+    if ( name == 'core/button' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          __experimentalBorder: false,
+          color: { "background": true, "text": false },
+        } ),
+      } );
+    }
+
+    if ( name == 'core/heading' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          color: { "background": false, "text": true },
+          typography: false,
+        } ),
+      } );
+    }
+
+    if ( name == 'core/list' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          color: { "background": true, "text": false },
+        } ),
+      } );
+    }
+
+    if ( name == 'core/group' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          color: { "background": true, "text": false },
+        } ),
+      } );
+    }
+
+    if ( name == 'core/columns' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          color: { "background": true, "text": false },
+        } ),
+      } );
+    }
+
+    if ( name == 'core/column' ) {
+
+      return lodash.assign( {}, settings, {
+        supports: lodash.assign( {}, settings.supports, {
+          color: { "background": true, "text": false },
+        } ),
+      } );
+    }
+
+    return settings;
+  }
+);
