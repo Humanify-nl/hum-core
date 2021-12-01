f043fff015358b4b38296f8d24c651a8a51f3620
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-12-01 16:21:20 +0100

    columns add testimonials

--- src/inc/theme-settings.php
+++ src/inc/theme-settings.php
@@ -10,6 +10,7 @@ include_once( get_template_directory() . '/inc/theme-settings/font.php');
 include_once( get_template_directory() . '/inc/theme-settings/admin.php');
 include_once( get_template_directory() . '/inc/theme-settings/admin-columns-posts.php');
 include_once( get_template_directory() . '/inc/theme-settings/admin-columns-pages.php');
+include_once( get_template_directory() . '/inc/theme-settings/admin-columns-testimonial.php');
 include_once( get_template_directory() . '/inc/theme-settings/customizer.php' );
 include_once( get_template_directory() . '/inc/theme-settings/preset-queries.php');
 include_once( get_template_directory() . '/inc/theme-settings/post-types.php');
---
+++ src/inc/theme-settings/admin-columns-testimonial.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Admin columns for posts
+ *
+ * @package hum-core
+ */
+
+/**
+ * Add columns for posts
+ *
+ */
+function hum_add_testimonial_img_column($columns) {
+
+  $columns = array_slice($columns, 0, 1, true) + array("img" => "small") + array_slice($columns, 1, count($columns) - 1, true);
+
+  return $columns;
+
+}
+add_filter('manage_testimonial_posts_columns', 'hum_add_testimonial_img_column' );
+
+
+/**
+ * Manage columns array for post
+ * -- added 'img'
+ *
+ */
+function hum_manage_testimonial_columns( $columns ) {
+
+  $columns = array(
+    'cb'                   => '<input type="checkbox" />',
+    'img'                  => 'Image',
+    'title'                => 'Title',
+    'date'                 => 'Date',
+  );
+
+  return $columns;
+}
+add_filter('manage_testimonial_posts_columns' , 'hum_manage_testimonial_columns' );
