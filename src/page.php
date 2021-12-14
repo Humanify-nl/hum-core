ebb17d44fc315c0271402668dab0215627fe9611
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-12-14 09:37:43 +0100

    add related pages

--- src/page.php
+++ src/page.php
@@ -7,6 +7,8 @@
 
 // Breadcrumbs
 add_action( 'tha_entry_top', 'hum_breadcrumbs', 8 );
+// Related posts
+add_action( 'tha_content_bottom', 'hum_pages_related' );
 
 //add_filter( 'hum_page_layout', 'hum_return_content_wide');
 
