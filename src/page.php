6cfa8158f368d0f03f5783c013492fbb01be7b89
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-12-23 11:38:43 +0100

    put layout filter to top

--- src/page.php
+++ src/page.php
@@ -5,12 +5,13 @@
  * @package hum-core
  */
 
+//add_filter( 'hum_page_layout', 'hum_return_content_wide');
+
 // Breadcrumbs
 add_action( 'tha_entry_top', 'hum_breadcrumbs', 8 );
 // Related posts
 add_action( 'tha_content_bottom', 'hum_pages_related' );
 
-//add_filter( 'hum_page_layout', 'hum_return_content_wide');
 
 // Build the page
 require get_template_directory() . '/index.php';
--- src/single.php
+++ src/single.php
@@ -5,20 +5,20 @@
  * @package hum-core
  */
 
+add_filter( 'hum_page_layout', 'hum_return_content_center' );
+
 add_action( 'tha_entry_top', 'hum_entry_title', 5 );
 add_action( 'tha_entry_top', 'hum_entry_category', 8 );
 add_action( 'tha_entry_top', 'hum_entry_excerpt', 10 );
 add_action( 'tha_entry_top', 'hum_entry_author', 12 );
 //add_action( 'tha_entry_top', 'hun_entry_header_share', 13 );
 
-add_filter( 'hum_page_layout', 'hum_return_content_center' );
-
 // Related posts
 add_action( 'tha_content_bottom', 'hum_posts_related' );
 
 // After entry
 add_action( 'tha_content_while_after', 'hum_single_after_entry', 8 );
-add_action( 'tha_content_while_after', 'hum_comments', 10 );
+//add_action( 'tha_content_while_after', 'hum_comments', 10 );
 
 // Build the page
 require get_template_directory() . '/index.php';
