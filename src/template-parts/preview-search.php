554d92d3297465d4845b2db1df382772305a5e2f
Robbert van Hooij <82579622+Humanify-nl@users.noreply.github.com>
2021-12-29 18:28:36 +0100

    search spacing fix & content template

---
+++ src/template-parts/preview-search.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Archive (preview)
+ *
+ * @package hum-core
+ */
+?>
+
+<article class="preview preview-post">
+
+  <?php
+  hum_preview_category();
+  hum_preview_title();
+  hum_preview_excerpt();
+  hum_preview_footer();
+  ?>
+
+</article>
